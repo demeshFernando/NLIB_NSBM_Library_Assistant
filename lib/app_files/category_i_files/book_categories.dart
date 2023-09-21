d285eec308120115813ce28467e02c634acb2f2f
Ahmed Afri <ahamedafricontact@gmail.com>
2023-09-21 19:53:22 +0530

    category page updated

--- lib/app_files/category_i_files/book_categories.dart
+++ lib/app_files/category_i_files/book_categories.dart
@@ -5,6 +5,13 @@ import 'package:nlib_library_assistant/utils/dimentions.dart';
 import '../../widgets/text_formatter.dart';
 import '../../utils/app_colors.dart';
 
+class BookCategory {
+  final String name;
+  final String imageAssetPath;
+
+  BookCategory(this.name, this.imageAssetPath);
+}
+
 class BookCategories extends StatefulWidget {
   const BookCategories({super.key});
 
@@ -14,6 +21,25 @@ class BookCategories extends StatefulWidget {
 }
 
 class _BookCategories extends State<BookCategories> {
+
+List<BookCategory> categories = [
+  BookCategory("Engineering", "asset/books/book_category/engineering.jpg"),
+  BookCategory("Biology", "asset/books/book_category/biology.jpg"),
+  BookCategory("Technology", "asset/books/book_category/computerscience.jpg"),
+  BookCategory("Business", "asset/books/book_category/business.jpg"),
+  BookCategory("Finance", "asset/books/book_category/finance.jpg"),
+  BookCategory("Law", "asset/books/book_category/law.jpg"),
+  BookCategory("Religion", "asset/books/book_category/religion.jpg"),
+  BookCategory("Travel", "asset/books/book_category/travel.jpg"),
+  BookCategory("Photography", "asset/books/book_category/photography.jpg"),
+  BookCategory("Astronomy", "asset/books/book_category/astronomy.jpg"),
+  BookCategory("Self Help", "asset/books/book_category/selfhelp.jpg"),
+  BookCategory("Philosophy", "asset/books/book_category/philosophy.jpg"),
+  
+  
+  // Add more categories here
+];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -32,35 +58,29 @@ class _BookCategories extends State<BookCategories> {
         ),
         title: const TextHeader(text: 'Book categories'),
       ),
-      body: SingleChildScrollView(
-        child: Column(
-          children: [
-            SizedBox(height: Dimentions.height40),
-            ListView.builder(
-                physics: const NeverScrollableScrollPhysics(),
-                shrinkWrap: true,
-                itemCount: 6,
-                itemBuilder: ((context, index) {
-                  return Container(
-                    margin: EdgeInsets.all(Dimentions.height10),
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        categoryCard(index),
-                      ],
-                    ),
-                  );
-                })),
-          ],
-        ),
+      body: ListView(
+        shrinkWrap: true,
+        children: [
+          GridView.count(
+            crossAxisCount: 2, // 2 columns
+            crossAxisSpacing: 1, // Spacing between columns
+            mainAxisSpacing: 1, // Spacing between rows
+            shrinkWrap: true,
+            physics: NeverScrollableScrollPhysics(), // Prevents scrolling inside the GridView
+            children: List.generate(categories.length, (index) {
+              return categoryCard(index,categories[index]);
+            }),
+          ),
+        ],
       ),
     );
   }
 
-  Widget categoryCard(int pageIndex) {
+  Widget categoryCard(int pageIndex,BookCategory category) {
     return Container(
       width: Dimentions.width300,
       height: Dimentions.height300,
+      margin: const EdgeInsets.fromLTRB(25, 15, 25, 15),
       decoration: BoxDecoration(
           borderRadius: BorderRadius.circular(Dimentions.radius10),
           color: AppColors.BASE_COLOR),
@@ -74,21 +94,21 @@ class _BookCategories extends State<BookCategories> {
             },
             child: Container(
               width: Dimentions.width250,
-              height: Dimentions.height150,
+              height: 110,
               decoration: BoxDecoration(
                   borderRadius: BorderRadius.circular(Dimentions.radius10),
                   color: AppColors.CONTAINER_WHITE),
               child: Image.asset(
-                "asset/books/book_category/default.png",
+                category.imageAssetPath,
                 fit: BoxFit.cover,
               ),
             ),
           ),
           SizedBox(
-            width: Dimentions.width150,
-            height: Dimentions.height50,
-            child: const Center(
-              child: BoldText(text: "Engineering"),
+            width: Dimentions.width100,
+            height: Dimentions.height30,
+            child: Center(
+              child: BoldText(text: category.name), // Adjust the text size by wrapping it in Text widget
             ),
           ),
         ],
